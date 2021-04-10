43175e549397acc60fa81a3bf728af7ae6207ec0
Gabster305 <u2create@gmail.com>
2021-04-10 15:31:48 -0400

    Made changes to functions

--- FUNC/nFunc.js
+++ FUNC/nFunc.js
@@ -5,10 +5,12 @@ let value2 = 2
 //Creating the function and naming it the action to do//
 //Giving the function parameter's. The are not the same name as the value assigned//
 function minusValue(x, y) {
-  //giving the function an action with let//
-  let result = x - y
-  //Display output will print what's inside ' ' + result //
+    //giving the function an action with let//
+    let result = x - y
+    //Display output will print what's inside ' ' + result //
   console.log('This is a name function minus Value ' + result)
-}
+} 
 //calling the function
 minusValue(value1, value2)
+// this is a change
+
