150ad71de860aae354d72ecafd9eb8103f861235
Gabster305 <u2create@gmail.com>
2021-04-11 22:07:18 -0400

    Explanation of an Array and structure

---
+++ ARRAY/arrays.js
@@ -0,0 +1,9 @@
+// Structure of an Array.
+let shoppingList = [ 'tomatoes','pears','tofu'];
+//console.log(shoppingList)// return 'tomatoes', 'pears, 'tofu'
+// Array [ list of items] Different type of items.
+// Unlike regular object like person that we assigned properties to
+// Arrays [ ] automatically receive properties. e.g. shoppingList.length 
+console.log(shoppingList.length);
+// this will return a 3. Number of items or elements
+// You can perform many operations in an array using the .notation
