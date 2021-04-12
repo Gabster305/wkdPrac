65b55a9c79529d7031c72913a3202ec5048c1bc9
Gabster305 <u2create@gmail.com>
2021-04-11 20:52:11 -0400

    variables samples

--- VARIABLE/variable.js
+++ VARIABLE/variable.js
@@ -1,4 +1,17 @@
-// Naming Variables
-let name = 'Gaby';
-console.log (name);
+// Naming Variables. Cannot use a reserved word and use a meaningful descriptive word.
+// Also can not start with a number. Cannot contain space or -
+// Case sensitive. Use Camel notation.
+// Declaring two variables. Best practice is to declare them in separate lines
+// let name = 'Gaby';
+// let lastName = 'Rios';
+
+let name = 'Gaby';// string lieteral
+let age = 30; // Number Literla
+let isApproved = false; // Boolean Literla
+let firsName = undefiled;
+let selectedColor = null; // when we want to clear value of a variable use null. null is object. 
+
+
+
+
 
