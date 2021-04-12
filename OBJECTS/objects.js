c2a0983c3954dbbcb330b763975b4bf9fa2d48ab
Gabster305 <u2create@gmail.com>
2021-04-11 21:25:59 -0400

    Turning the Variables into the properties of an object. e.g. Person

---
+++ OBJECTS/objects.js
@@ -0,0 +1,22 @@
+// Objects are like objects in real life. e.g. a person.
+// A person has a name, age, address, phone, email ...etc.
+// Instead of assigning all these as variable separately e.g let name = 'Gaby';
+// We make this variables properties of the object Person.
+// The syntax to an object 
+    let person = {
+    name: 'Gaby',
+    age: 35
+};
+//console.log(person)  //Here we receive all the properties of that object
+ // If we want to receive only certain properties of that object then we use 
+ // Dot Notation e.g.
+ console.log (person.name) // we only receive Gaby
+
+ // Bracket Notation e.g.
+ // let selection = 'name';
+ // person[name] = 'Mary';
+ //
+ // console.log(person.name);
+
+
+
